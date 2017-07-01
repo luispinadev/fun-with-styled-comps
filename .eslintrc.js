137cbc0ed046c1b97e363e8d4f14eb2208b4cb31
Pina <luispinadev@gmail.com>
2017-07-01 23:12:40 +0200

    initial commit from cra (+ custom eslintrc)

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+
+  "root": true,
+
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true
+  },
+
+  // "parser": "babel-eslint",
+
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+      "classes": true
+    }
+  },
+
+  "plugins": [
+    //  "babel",
+     "react",
+     "import"
+  ],
+
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+
+  "rules" : {
+    "no-console": [ "warn", { "allow": ["error", "warn"] } ],
+    "max-len": ["error", 80, { "ignoreComments": true }],
+    "no-unused-vars": "warn"
+  },
+
+  "globals": {
+    "jest": true,
+    "expect": true,
+    "describe": true,
+    "it": true,
+    "beforeEach": true,
+    "before": true,
+    "after": true,
+  }
+
+}
