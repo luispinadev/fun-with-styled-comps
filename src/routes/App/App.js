5f326cc3d247be3bd831b9832e2165f566224634
Pina <luispinadev@gmail.com>
2017-07-02 01:37:04 +0200

    Cleanup App component

--- src/routes/App/App.js
+++ src/routes/App/App.js
@@ -1,24 +1,19 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 import Container from './Container'
 import Header from './Header'
 import Intro from './Intro'
 import Logo from './Logo'
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Header>
-          <Logo />
-          <h2>Welcome to React</h2>
-        </Header>
-        <Intro>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </Intro>
-      </Container>
-    )
-  }
-}
+const App = () =>
+  <Container>
+    <Header>
+      <Logo />
+      <h2>Welcome to React</h2>
+    </Header>
+    <Intro>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Intro>
+  </Container>
 
 export default App
